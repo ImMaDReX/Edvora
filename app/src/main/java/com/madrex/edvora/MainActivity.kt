be6d930b9d62a37702eb5ef4137e389578d1be2c
Aman Bhatt <stark4498@gmail.com>
2022-03-29 14:01:38 +0530

    Edvora

---
+++ app/src/main/java/com/madrex/edvora/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.madrex.edvora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
